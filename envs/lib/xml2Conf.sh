43316fefbf359c4193f1cd8d5bd23a2e03b90e8d
cabralh <hjcbr19@gmail.com>
2020-10-27 21:02:33 -0400

    initial environment notebook creation

---
+++ envs/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/lib"
+XML2_LIBS="-lxml2 -L/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/lib -lz -L/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/lib -llzma -lpthread -L/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/lib -L/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/include/libxml2 -I/home/cabralh/Documents/Projects/Machine-Learning/WS-Predictor/envs/include"
+MODULE_VERSION="xml2-2.9.10"
+
